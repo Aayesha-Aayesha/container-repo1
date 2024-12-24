f00363c45969a7e3eddf98dec824360f9f5f3d28
Aayesha-Aayesha <153555591+Aayesha-Aayesha@users.noreply.github.com>
2024-12-24 16:55:22 +0000

    Added text content in the web page.

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello, World!"
+    return "Hello, World! I am Aayesha, New in Azure :)"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
